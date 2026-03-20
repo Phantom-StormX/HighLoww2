9fdd3fad7dc75a010e0d3880154a8f695edfebc9
impichler <impichler@thewildcats.net>
2026-03-20 11:16:24 -0400

    added GUI

---
+++ src/BorderGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class BorderGUI {
+    private JPanel mainPanel;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JPanel centerPanel;
+    private JButton button6;
+    private JButton button7;
+    private JLabel BANANAPEPPER;
+    private JTextField RIGATONIPASTATextField;
+    private JTable TABLE;
+}
